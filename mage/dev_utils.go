55a5e4d0a186333fafef5a2bf0901ee45ab0fd12
Yermolenko, Dmytro <dmytro.yermolenko@intel.com>
2025-04-08 16:37:49 +0200

    Some Go lint suggestions

--- mage/dev_utils.go
+++ mage/dev_utils.go
@@ -405,7 +405,7 @@ func ManageRoles(cli *http.Client, token, action, username, orgUUID, projUUID st
 		return fmt.Errorf("failed to get user ID: %w", err)
 	}
 
-	pollIntervalInSecs := time.Duration(1)
+	pollIntervalInSecs := time.Duration.Seconds(1)
 	retryCount := 120 // wait for 2 mins
 	for i := 0; i < retryCount; i++ {
 		var errs []error
@@ -418,7 +418,7 @@ func ManageRoles(cli *http.Client, token, action, username, orgUUID, projUUID st
 			return nil
 		}
 		fmt.Printf("Not all roles are assigned, failed with an error: %v, Retrying...", errs)
-		time.Sleep(pollIntervalInSecs * time.Second)
+		time.Sleep(time.Duration(pollIntervalInSecs))
 	}
 	return fmt.Errorf("time out waiting for roles to be assigned")
 }
