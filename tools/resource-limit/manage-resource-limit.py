a49a8d4ba2cc2f687f2ba8830d850d739d42c641
Tseng, Yi <yi.tseng@intel.com>
2025-04-15 00:12:48 -0700

    more fixes

--- tools/resource-limit/manage-resource-limit.py
+++ tools/resource-limit/manage-resource-limit.py
@@ -127,7 +127,7 @@ def get_resource_config(namespace):
     mapping = load_mappings()
     metrics = get_metrics(namespace)
     resoruce_configs = {} # key -> requests and limits
-    if pod_prefixes not in mapping:
+    if namespace not in mapping:
         return {}, { "cpu_request": 0, "memory_request": 0, "cpu_limit": 0, "memory_limit": 0 }
     pod_prefixes = mapping.get(namespace, {})
     for item in metrics:
