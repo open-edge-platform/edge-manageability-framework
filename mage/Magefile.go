4c3b1f7ce1554fae10abe430cda8a30391159f8d
Matt Balvin <matthew.d.balvin@intel.com>
2025-04-16 00:51:00 -0700

    Implement ITEP-1533 - Add support for deploy container trivvy scan
    - Add mage command to get image tag
    - Add workflow step that scans the installer image
    
    - Temp: Disable merge conditionals for validation testing

--- mage/Magefile.go
+++ mage/Magefile.go
@@ -1858,6 +1858,15 @@ type LogUtils mg.Namespace
 
 type Version mg.Namespace
 
+func (Version) GetVersionTag() error {
+	tag, err := getDeployTag()
+	if err != nil {
+		return fmt.Errorf("failed to get deploy tag: %w", err)
+	}
+	fmt.Println(tag)
+	return nil
+}
+
 // Checks that the Version in the VERSION file and in the Argo Charts is in syn
 func (v Version) CheckVersion() error {
 	return v.checkVersion()
