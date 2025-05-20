5acda5a7fa298decbeaabb4b0df55228e654969f
Tseng, Yi <yi.tseng@intel.com>
2025-05-20 15:29:06 -0700

    Add mage command to build new installer

---
+++ mage/new_installer.go
@@ -0,0 +1,22 @@
+package mage
+
+import (
+	"fmt"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+type NewInstaller mg.Namespace
+
+func (NewInstaller) Build() error {
+	if err := sh.RunV("mkdir", "-p", "new-installer/_build"); err != nil {
+		return err
+	}
+	// Build the new installer binary
+	if err := sh.RunV("go", "build", "-C", "new-installer", "-o", "_build/orch-installer", "./cmd"); err != nil {
+		return err
+	}
+	fmt.Println("Installer built successfully. Run ./new-installer/_build/orch-installer to start the installer.")
+	return nil
+}
