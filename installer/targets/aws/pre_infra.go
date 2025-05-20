0ebc59ef05c4ed0ae2868cbaad39d328201566f4
Tseng, Yi <yi.tseng@intel.com>
2025-05-20 13:29:41 -0700

    Run all steps in PreInfra

--- installer/targets/aws/pre_infra.go
+++ installer/targets/aws/pre_infra.go
@@ -13,71 +13,63 @@ type PreInfraStage struct {
 	RootPath string
 	// Keeps the generated files such as Terraform variables and backend config.
 	KeepGeneratedFiles bool
+
+	steps []steps.OrchInstallerStep
+}
+
+func NewPreInfraStage(rootPath string, keepGeneratedFiles bool) *PreInfraStage {
+	return &PreInfraStage{
+		RootPath:           rootPath,
+		KeepGeneratedFiles: keepGeneratedFiles,
+		steps: []steps.OrchInstallerStep{
+			&steps.CreateAWSStateBucket{},
+			&steps.AWSVPCStep{
+				RootPath:           rootPath,
+				KeepGeneratedFiles: keepGeneratedFiles,
+			},
+		},
+	}
 }
 
 func (a *PreInfraStage) Name() string {
 	return "PreInfraStage"
 }
 func (a *PreInfraStage) PreStage(ctx context.Context, config internal.OrchInstallerConfig, runtimeState *internal.OrchInstallerRuntimeState) *internal.OrchInstallerError {
-	createAWSStateBucketStep := &steps.CreateAWSStateBucket{}
-	err := func() *internal.OrchInstallerError {
-		if newRuntimeState, err := createAWSStateBucketStep.ConfigStep(ctx, config, *runtimeState); err != nil {
+	for _, step := range a.steps {
+
+		if newRuntimeState, err := step.ConfigStep(ctx, config, *runtimeState); err != nil {
 			return err
 		} else if err = runtimeState.UpdateRuntimeState(newRuntimeState); err != nil {
 			return err
 		}
 
-		if newRuntimeState, err := createAWSStateBucketStep.PreSetp(ctx, config, *runtimeState); err != nil {
+		if newRuntimeState, err := step.PreSetp(ctx, config, *runtimeState); err != nil {
 			return err
 		} else if err = runtimeState.UpdateRuntimeState(newRuntimeState); err != nil {
 			return err
 		}
-		if newRuntimeState, err := createAWSStateBucketStep.RunStep(ctx, config, *runtimeState); err != nil {
-			return err
-		} else if err = runtimeState.UpdateRuntimeState(newRuntimeState); err != nil {
-			return err
-		}
-		return nil
-	}()
-	if newRuntimeState, err := createAWSStateBucketStep.PostStep(ctx, config, *runtimeState, err); err != nil {
-		return err
-	} else {
-		return runtimeState.UpdateRuntimeState(newRuntimeState)
 	}
+	return nil
 }
 
 func (a *PreInfraStage) RunStage(ctx context.Context, config internal.OrchInstallerConfig, runtimeState *internal.OrchInstallerRuntimeState) *internal.OrchInstallerError {
-	vpcStep := &steps.AWSVPCStep{
-		RootPath:           a.RootPath,
-		KeepGeneratedFiles: a.KeepGeneratedFiles,
+	for _, step := range a.steps {
+		if newRuntimeState, err := step.RunStep(ctx, config, *runtimeState); err != nil {
+			return err
+		} else if err = runtimeState.UpdateRuntimeState(newRuntimeState); err != nil {
+			return err
+		}
 	}
-	err := func() *internal.OrchInstallerError {
-		if newRuntimeState, err := vpcStep.ConfigStep(ctx, config, *runtimeState); err != nil {
-			return err
-		} else if err = runtimeState.UpdateRuntimeState(newRuntimeState); err != nil {
-			return err
-		}
-
-		if newRuntimeState, err := vpcStep.PreSetp(ctx, config, *runtimeState); err != nil {
-			return err
-		} else if err = runtimeState.UpdateRuntimeState(newRuntimeState); err != nil {
-			return err
-		}
-
-		if newRuntimeState, err := vpcStep.RunStep(ctx, config, *runtimeState); err != nil {
-			return err
-		} else if err = runtimeState.UpdateRuntimeState(newRuntimeState); err != nil {
-			return err
-		}
-		return nil
-	}()
-	if newRuntimeState, err := vpcStep.PostStep(ctx, config, *runtimeState, err); err != nil {
-		return err
-	} else {
-		return runtimeState.UpdateRuntimeState(newRuntimeState)
-	}
+	return nil
 }
 
 func (a *PreInfraStage) PostStage(ctx context.Context, config internal.OrchInstallerConfig, runtimeState *internal.OrchInstallerRuntimeState, prevStageError *internal.OrchInstallerError) *internal.OrchInstallerError {
-	return prevStageError
+	for _, step := range a.steps {
+		if newRuntimeState, err := step.PostStep(ctx, config, *runtimeState, prevStageError); err != nil {
+			return err
+		} else if err = runtimeState.UpdateRuntimeState(newRuntimeState); err != nil {
+			return err
+		}
+	}
+	return nil
 }
