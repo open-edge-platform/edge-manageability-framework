6bf9354bc82209b2cb5696954104534893170ee5
Tseng, Yi <yi.tseng@intel.com>
2025-05-21 16:42:16 -0700

    Add VPC steps

--- new-installer/targets/aws/aws.go
+++ new-installer/targets/aws/aws.go
@@ -7,11 +7,18 @@ package aws
 import (
 	"github.com/open-edge-platform/edge-manageability-framework/installer/internal"
 	"github.com/open-edge-platform/edge-manageability-framework/installer/internal/steps"
+	steps_aws "github.com/open-edge-platform/edge-manageability-framework/installer/internal/steps/aws"
 )
 
 func CreateAWSStages(rootPath string, keepGeneratedFiles bool) []internal.OrchInstallerStage {
 	return []internal.OrchInstallerStage{
-		NewAWSStage("PreInfra", []steps.OrchInstallerStep{}),
+		NewAWSStage("PreInfra", []steps.OrchInstallerStep{
+			&steps_aws.CreateAWSStateBucket{},
+			&steps_aws.AWSVPCStep{
+				RootPath:           rootPath,
+				KeepGeneratedFiles: keepGeneratedFiles,
+			},
+		}),
 		NewAWSStage("Infra", []steps.OrchInstallerStep{}),
 		NewAWSStage("PreOrch", []steps.OrchInstallerStep{}),
 		NewAWSStage("Orch", []steps.OrchInstallerStep{}),
