7a7ace254a5f880276f28f564f5b5836bd3ebf50
Tseng, Yi <yi.tseng@intel.com>
2025-05-22 09:15:19 -0700

    Add steps to AWS PreInfra stage

--- new-installer/targets/aws/aws.go
+++ new-installer/targets/aws/aws.go
@@ -11,10 +11,19 @@ import (
 )
 
 func CreateAWSStages(rootPath string, keepGeneratedFiles bool) ([]internal.OrchInstallerStage, error) {
+	tfExecPath, err := steps.InstallTerraformAndGetExecPath()
+	if err != nil {
+		return nil, err
+	}
 	return []internal.OrchInstallerStage{
 		NewAWSStage("PreInfra", []steps.OrchInstallerStep{
-			&steps_aws.CreateAWSStateBucket{},
+			&steps_aws.CreateAWSStateBucket{
+				TerraformExecPath:  tfExecPath,
+				RootPath:           rootPath,
+				KeepGeneratedFiles: keepGeneratedFiles,
+			},
 			&steps_aws.AWSVPCStep{
+				TerraformExecPath:  tfExecPath,
 				RootPath:           rootPath,
 				KeepGeneratedFiles: keepGeneratedFiles,
 			},
