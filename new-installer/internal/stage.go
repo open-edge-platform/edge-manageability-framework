7544818ed8fd260c68dfd5cb511704cc456cc0fc
Tseng, Yi <yi.tseng@intel.com>
2025-05-23 10:22:28 -0700

    simplify stages and add labels

--- new-installer/internal/stage.go
+++ new-installer/internal/stage.go
@@ -12,15 +12,17 @@ import (
 
 type OrchInstallerStage interface {
 	Name() string
+	// Labels for the stage
+	Labels() []string
 	// PreStage: initialize the stage, such as creating directories, downloading files, etc.
 	// It also process the output/runtime-state from previous stage.
-	PreStage(ctx context.Context, config *config.OrchInstallerConfig) *OrchInstallerStageError
+	PreStage(ctx context.Context, config *config.OrchInstallerConfig) *OrchInstallerError
 
 	// RunStage: run the stage, such as running terraform, ansible, etc.
-	RunStage(ctx context.Context, config *config.OrchInstallerConfig) *OrchInstallerStageError
+	RunStage(ctx context.Context, config *config.OrchInstallerConfig) *OrchInstallerError
 
 	// PostStage: cleanup the stage, such as removing directories, files, etc.
 	// It should also handle the error from the previous stage and rollback if needed.
 	// It should also return the final output of the stage.
-	PostStage(ctx context.Context, config *config.OrchInstallerConfig, prevStageError *OrchInstallerStageError) *OrchInstallerStageError
+	PostStage(ctx context.Context, config *config.OrchInstallerConfig, prevStageError *OrchInstallerError) *OrchInstallerError
 }
