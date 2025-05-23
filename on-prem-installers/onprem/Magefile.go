cffb405293c7dc18501a7d0e09e7e5a564dfb5c9
OLoughlin, John <john.oloughlin@intel.com>
2025-05-23 04:09:54 -0700

    first mage commit
    Signed-off-by: OLoughlin, John <john.oloughlin@intel.com>

---
+++ on-prem-installers/onprem/Magefile.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2025 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build mage
+
+package main
+
+import (
+        // mage:import
+        . "github.com/open-edge-platform/edge-manageability-framework/onprem"//nolint: revive
+)
+
+type OnPrem mg.Namespace
