68b587d5d0c396512b6098a63c2e90f6bc2caf92
Tseng, Yi <yi.tseng@intel.com>
2025-05-23 14:46:08 -0700

    Remove changes to infra-stages

--- new-installer/targets/aws/aws.go
+++ new-installer/targets/aws/aws.go
@@ -8,29 +8,11 @@ import (
 	"github.com/open-edge-platform/edge-manageability-framework/installer/internal"
 	"github.com/open-edge-platform/edge-manageability-framework/installer/internal/config"
 	"github.com/open-edge-platform/edge-manageability-framework/installer/internal/steps"
-	steps_aws "github.com/open-edge-platform/edge-manageability-framework/installer/internal/steps/aws"
 )
 
 func CreateAWSStages(rootPath string, keepGeneratedFiles bool, orchConfigReaderWriter config.OrchConfigReaderWriter) ([]internal.OrchInstallerStage, error) {
-	tfExecPath, err := steps.InstallTerraformAndGetExecPath()
-	if err != nil {
-		return nil, err
-	}
 	return []internal.OrchInstallerStage{
-		NewAWSStage("PreInfra", []steps.OrchInstallerStep{
-			&steps_aws.CreateAWSStateBucket{
-				TerraformExecPath:  tfExecPath,
-				RootPath:           rootPath,
-				KeepGeneratedFiles: keepGeneratedFiles,
-				StepLabels:         []string{"pre-infra", "state-bucket"},
-			},
-			&steps_aws.AWSVPCStep{
-				TerraformExecPath:  tfExecPath,
-				RootPath:           rootPath,
-				KeepGeneratedFiles: keepGeneratedFiles,
-				StepLabels:         []string{"pre-infra", "vpc"},
-			},
-		}, []string{"pre-infra"}, orchConfigReaderWriter),
+		NewAWSStage("PreInfra", []steps.OrchInstallerStep{}, []string{"pre-infra"}, orchConfigReaderWriter),
 		NewAWSStage("Infra", []steps.OrchInstallerStep{}, []string{"infra"}, orchConfigReaderWriter),
 	}, nil
 }
