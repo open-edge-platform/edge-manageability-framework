4cbd2f166ea83c58db293e560609f938d348329a
OLoughlin, John <john.oloughlin@intel.com>
2025-05-26 06:16:06 -0700

    test
    Signed-off-by: OLoughlin, John <john.oloughlin@intel.com>

--- on-prem-installers/onprem/onprem_installer.sh
+++ on-prem-installers/onprem/onprem_installer.sh
@@ -34,6 +34,7 @@ source "$(dirname "$0")/functions.sh"
 wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+export PATH=${PATH}:`go env GOPATH`/bin
 cd ../
 go install github.com/asdf-vm/asdf/cmd/asdf@v0.17.0
 asdf plugin add mage
