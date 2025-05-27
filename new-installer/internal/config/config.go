f1837c39edac1ef9da621636985d85159a37d776
Tseng, Yi <yi.tseng@intel.com>
2025-05-27 16:00:41 -0700

    Check schema version

--- new-installer/internal/config/config.go
+++ new-installer/internal/config/config.go
@@ -16,8 +16,8 @@ const (
 	Scale10000 Scale = 10000
 )
 
-// The schema version for the OrchInstallerRuntimeState and OrchInstallerConfig
-const SchemaVersion = 1
+const ConfigSchemaVersion = 1
+const RuntimeStateSchemaVersion = 1
 
 type OrchInstallerRuntimeState struct {
 	Version int `yaml:"version"`
