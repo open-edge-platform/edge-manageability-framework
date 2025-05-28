c5fc372230991c0c61eefd3acca02ad198121550
Charles Chan <425857+charlesmcchan@users.noreply.github.com>
2025-05-28 08:38:10 -0700

    Interactive Config Builder for Orchestrator Installer (#349)

--- mage/new_installer.go
+++ mage/new_installer.go
@@ -6,6 +6,8 @@ package mage
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
@@ -13,33 +15,124 @@ import (
 
 type NewInstaller mg.Namespace
 
+var (
+	rootDir        = "new-installer"
+	buildDir       = "_build"
+	coverProfile   = "coverprofile.out"
+	coverageReport = "coverage.txt"
+)
+
 func (NewInstaller) Build() error {
-	if err := sh.RunV("mkdir", "-p", "new-installer/_build"); err != nil {
-		return err
+	if (NewInstaller{}).BuildInstaller() != nil {
+		return fmt.Errorf("failed to build installer")
 	}
+	if (NewInstaller{}).BuildConfigBuilder() != nil {
+		return fmt.Errorf("failed to build config builder")
+	}
+	return nil
+}
+
+func (NewInstaller) BuildInstaller() error {
+	output := filepath.Join(buildDir, "orch-installer")
+	if err := buildInternal("./cmd", output); err != nil {
+		return fmt.Errorf("failed to build config builder: %w", err)
+	}
+	return nil
+}
+
+func (NewInstaller) BuildConfigBuilder() error {
+	output := filepath.Join(buildDir, "config-builder")
+	if err := buildInternal("./cmd/config", output); err != nil {
+		return fmt.Errorf("failed to build config builder: %w", err)
+	}
+	return nil
+}
+
+func buildInternal(srcFolder string, output string) error {
+	// Ensure the build directory exists
+	if err := os.MkdirAll(filepath.Join(rootDir, buildDir), 0o755); err != nil {
+		return fmt.Errorf("failed to create build directory: %w", err)
+	}
+
 	// Build the new installer binary
-	if err := sh.RunV("go", "build", "-C", "new-installer", "-o", "_build/orch-installer", "./cmd"); err != nil {
+	if err := sh.RunV("go", "build", "-C", rootDir, "-o", output, srcFolder); err != nil {
 		return err
 	}
-	fmt.Println("Installer built successfully. Run ./new-installer/_build/orch-installer to start the installer.")
+	fmt.Printf("Installer built successfully. Run %s to start the installer.\n", filepath.Join(rootDir, output))
 	return nil
 }
 
 func (NewInstaller) Test() error {
+	if err := os.Chdir(rootDir); err != nil {
+		return fmt.Errorf("failed to change directory to %s: %w", rootDir, err)
+	}
+
+	defer func() {
+		if err := os.Chdir(".."); err != nil {
+			fmt.Printf("Warning: failed to change back to root directory: %v\n", err)
+		}
+	}()
+
 	// Run tests for the new installer, except for the AWS IaC tests
 	// Ginkgo flags:
 	// -v: verbose output
 	// -r: recursive test
 	// -p: parallel test
 	// --skip-package: skip tests in specific packages
-	if err := sh.RunV("ginkgo", "-v", "-r", "-p", "--skip-package=new-installer/targets/aws/iac", "new-installer"); err != nil {
+	if err := sh.RunV("ginkgo", "-v", "-r", "-p",
+		"--skip-package=new-installer/targets/aws/iac",
+		"--cover"); err != nil {
 		return err
 	}
-	fmt.Println("Installer tests passed successfully.")
+
+	if _, err := os.Stat(coverProfile); err == nil {
+		if err := sh.RunV("go", "tool", "cover", "-func="+coverProfile, "-o", coverageReport); err != nil {
+			return fmt.Errorf("failed to generate coverage report: %w", err)
+		}
+		fmt.Printf("Coverage report saved to %s\n", coverageReport)
+	}
 	return nil
 }
 
 // Test Terraform modules
 func (NewInstaller) TestIaC() error {
+	return nil
+}
+
+func (NewInstaller) Clean() error {
+	// Remove the build directory
+	dir := filepath.Join(rootDir, buildDir)
+	if err := os.RemoveAll(dir); err != nil {
+		return fmt.Errorf("failed to clean build directory %s: %w", dir, err)
+	}
+
+	// Remove the cover profile file if it exists
+	file := filepath.Join(rootDir, coverProfile)
+	if err := os.Remove(file); err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("failed to remove %s: %w", file, err)
+	}
+
+	// Remove the cover profile file if it exists
+	file = filepath.Join(rootDir, coverageReport)
+	if err := os.Remove(file); err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("failed to remove %s: %w", file, err)
+	}
+
+	// Remove all *.test files
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".test" {
+			if removeErr := os.Remove(path); removeErr != nil && !os.IsNotExist(removeErr) {
+				return fmt.Errorf("failed to remove %s: %w", path, removeErr)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed to remove *.test files: %w", err)
+	}
+
 	return nil
 }
