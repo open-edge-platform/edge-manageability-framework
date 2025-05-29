132f0e0669ff8647a140d6202dfb7636ee3a8f82
Charles Chan <charles.chan@intel.com>
2025-05-28 22:36:50 -0700

    fix user name

--- pod-configs/orchestrator/cluster/variable.tf
+++ pod-configs/orchestrator/cluster/variable.tf
@@ -218,7 +218,7 @@ variable "orch_databases" {
     }
     "rps" : {
       namespace : "orch-infra"
-      user: "orch-infra-system-mps_user"
+      user: "orch-infra-system-rps_user"
     }
     # Uncomment following when using RDS for Gitea
     # "gitea": {
