e88816e810dc73d5c4c9887e0632a256466f7411
Tseng, Yi <yi.tseng@intel.com>
2025-06-05 21:35:28 -0700

    include EKS oidc issuer to output

--- new-installer/targets/aws/iac/eks/output.tf
+++ new-installer/targets/aws/iac/eks/output.tf
@@ -1,1 +1,8 @@
-# This file is intentionally left blank.
+# SPDX-FileCopyrightText: 2025 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+output "eks_oidc_issuer" {
+  description = "The OIDC issuer URL for the EKS cluster."
+  value = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
