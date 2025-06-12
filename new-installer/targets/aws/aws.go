e631182629b4758ed94b6356b4f1d982de8fe05d
Tseng, Yi <yi.tseng@intel.com>
2025-06-12 16:03:40 -0700

    Disable EFS for now until we get EKS included

--- new-installer/targets/aws/aws.go
+++ new-installer/targets/aws/aws.go
@@ -59,7 +59,7 @@ func CreateAWSStages(rootPath string, keepGeneratedFiles bool, orchConfigReaderW
 			steps_aws.CreateVPCStep(rootPath, keepGeneratedFiles, tfUtil, aws_util),
 		}, []string{"pre-infra"}, orchConfigReaderWriter),
 		NewAWSStage("Infra", []steps.OrchInstallerStep{
-			steps_aws.CreateEFSStep(rootPath, keepGeneratedFiles, tfUtil, aws_util),
+			// steps_aws.CreateEFSStep(rootPath, keepGeneratedFiles, tfUtil, aws_util),
 			steps_aws.CreateObservabilityBucketsStep(rootPath, keepGeneratedFiles, tfUtil, aws_util),
 			steps_aws.CreateKMSStep(rootPath, keepGeneratedFiles, tfUtil, aws_util),
 		}, []string{"infra"}, orchConfigReaderWriter),
