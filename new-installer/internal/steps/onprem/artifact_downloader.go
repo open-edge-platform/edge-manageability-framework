bb6722cd5a871a092ebb068eb6c9318fecc12229
Andrei Palade <andrei.palade@intel.com>
2025-06-16 09:44:16 +0100

    Download rke2 images (#487)

--- new-installer/internal/steps/onprem/artifact_downloader.go
+++ new-installer/internal/steps/onprem/artifact_downloader.go
@@ -7,7 +7,9 @@ package onprem
 import (
 	"context"
 	"fmt"
+	"net/url"
 	"os"
+	"os/exec"
 
 	"github.com/open-edge-platform/edge-manageability-framework/installer/internal"
 	"github.com/open-edge-platform/edge-manageability-framework/installer/internal/config"
@@ -22,6 +24,14 @@ const (
 	ARCHIVES_RS_PATH   = "edge-orch/common/files/orchestrator"
 	ORCH_VERSION       = "3.1.0-dev-eca1939"
 	INSTALLERS_DIR     = "/tmp/installers"
+
+	rke2Version        = "v1.30.10+rke2r1"
+	rke2Binary         = "rke2.linux-amd64.tar.gz"
+	rke2ImagesPkg      = "rke2-images.linux-amd64.tar.zst"
+	rke2CalicoImagePkg = "rke2-images-calico.linux-amd64.tar.zst"
+	rke2LibSHAFile     = "sha256sum-amd64.txt"
+	rke2ImagesUrl      = "https://github.com/rancher/rke2/releases/download"
+	rke2InstallerUrl   = "https://get.rke2.io"
 )
 
 var installerList = []string{
@@ -75,6 +85,15 @@ func (s *ArtifactDownloader) RunStep(ctx context.Context, config config.OrchInst
 
 		fmt.Println("Created directories for installers")
 
+		if err := downloadRKE2(ctx, INSTALLERS_DIR); err != nil {
+			return runtimeState, &internal.OrchInstallerError{
+				ErrorMsg:  fmt.Sprintf("failed to download RKE2: %s", err),
+				ErrorCode: internal.OrchInstallerErrorCodeInternal,
+			}
+		}
+
+		fmt.Println("RKE2 images and install script downloaded successfully")
+
 		if err := downloadArtifacts(ctx, RS_URL, INSTALLERS_RS_PATH, ORCH_VERSION, INSTALLERS_DIR, installerList); err != nil {
 			return runtimeState, &internal.OrchInstallerError{
 				ErrorCode: internal.OrchInstallerErrorCodeInternal,
@@ -84,6 +103,7 @@ func (s *ArtifactDownloader) RunStep(ctx context.Context, config config.OrchInst
 
 		fmt.Println("Downloaded installers successfully")
 	}
+
 	return runtimeState, nil
 }
 
@@ -111,5 +131,38 @@ func downloadArtifacts(ctx context.Context, registryUrl, registryPath, orchVersi
 		}
 		fmt.Println("manifest descriptor:", manifestDescriptor)
 	}
+	return nil
+}
+
+func downloadRKE2(ctx context.Context, artifactDir string) error {
+	fmt.Println("Downloading RKE2 images and install script...")
+	rke2VersionEscaped := url.QueryEscape(rke2Version)
+
+	// Download the RKE2 images and binaries
+	for _, image := range []string{
+		rke2Binary,
+		rke2ImagesPkg,
+		rke2CalicoImagePkg,
+		rke2LibSHAFile,
+	} {
+		rke2DownloadURL := fmt.Sprintf("%s/%s/%s", rke2ImagesUrl, rke2VersionEscaped, image)
+		fmt.Printf("Downloading RKE2 from %s\n", rke2DownloadURL)
+		cmd := exec.CommandContext(ctx, "curl", "-L", rke2DownloadURL, "-o", fmt.Sprintf("%s/%s", artifactDir, image))
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("failed to download RKE2 image %s: %w", image, err)
+		}
+	}
+
+	// Download the RKE2 install script
+	fmt.Printf("Downloading RKE2 install script from %s\n", rke2InstallerUrl)
+	cmd := exec.CommandContext(ctx, "curl", "-sfL", rke2InstallerUrl, "-o", fmt.Sprintf("%s/%s", artifactDir, "install.sh"))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to download install script: %w", err)
+	}
+
 	return nil
 }
