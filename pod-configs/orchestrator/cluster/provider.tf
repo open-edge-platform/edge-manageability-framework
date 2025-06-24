54534c389ae62a258bfbecb876f45f3805a9379b
tejaswin98 <tejaswini.nayak@intel.com>
2025-06-24 10:03:07 +0530

    fix the helm provider

--- pod-configs/orchestrator/cluster/provider.tf
+++ pod-configs/orchestrator/cluster/provider.tf
@@ -31,23 +31,8 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster_data.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster_data.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
-    command     = "aws"
-  }
+  config_path = "~/.kube/config"
 }
 
 provider "helm" {
-  kubernetes = {
-    host                   = data.aws_eks_cluster.eks_cluster_data.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster_data.certificate_authority[0].data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
-      command     = "aws"
-    }
-  }
 }
