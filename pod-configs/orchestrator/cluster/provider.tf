f2a86c08a999f9d27b8b9f302b5c6b385415b334
Tseng, Yi <yi.tseng@intel.com>
2025-06-23 22:27:06 -0700

    Minor fix

--- pod-configs/orchestrator/cluster/provider.tf
+++ pod-configs/orchestrator/cluster/provider.tf
@@ -48,7 +48,7 @@ provider "helm" {
   kubernetes = {
     host                   = data.aws_eks_cluster.eks_cluster_data.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster_data.certificate_authority[0].data)
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
       command     = "aws"
