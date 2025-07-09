732e8a66823f91604816982bb2e03d3f23701d3a
Tomasz Osinski <tomasz.osinski@intel.com>
2025-07-09 17:45:53 +0200

    feat: Enable vEN onboarding test for OXM profile

--- mage/Magefile.go
+++ mage/Magefile.go
@@ -919,7 +919,7 @@ func (d Deploy) VENWithFlow(ctx context.Context, flow string, serialNumber strin
 		return fmt.Errorf("failed to change directory to 'ven': %w", err)
 	}
 
-	if err := sh.RunV("git", "checkout", "pico/1.5.2"); err != nil {
+	if err := sh.RunV("git", "checkout", "pico/1.5.3"); err != nil {
 		return fmt.Errorf("failed to checkout specific commit: %w", err)
 	}
 
