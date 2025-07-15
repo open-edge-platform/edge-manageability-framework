bf6dd61e46778cbe09fc1cb7b2dc822712bd87eb
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2025-07-14 23:08:21 -0700

    remove edge node failing test

--- e2e-tests/orchestrator/o11y_edgenode_test.go
+++ e2e-tests/orchestrator/o11y_edgenode_test.go
@@ -138,19 +138,6 @@ var _ = Describe("Edgenode Observability Test:", Ordered, Label(edgenodeObs), fu
 			Expect(resp.StatusCode).To(Equal(http.StatusOK))
 		})
 
-		It("Check if metrics started coming from edgenode", func() {
-			Eventually(func() error {
-				found, err := helpers.CheckMetric(cli, metricsAddr, "cpu_usage_idle", projectID)
-				if err != nil {
-					return err
-				}
-				if found {
-					return nil
-				}
-				return errors.New("metrics are not present yet")
-			}, 10*time.Minute, 20*time.Second).Should(Succeed(), "eventually metrics should come from ENiC")
-		})
-
 		It("Edgenode metrics must be present in mimir", func() {
 			for _, metric := range enMetrics {
 				found, err := helpers.CheckMetric(cli, metricsAddr, metric, projectID)
