76de4689ba8b50e01aa754dbc78683b993191634
Sunil Parida <sunil.kumar.parida@intel.com>
2025-07-16 21:37:23 +0530

    onprem upgrade fix shell check (#576)

--- on-prem-installers/onprem/onprem_upgrade.sh
+++ on-prem-installers/onprem/onprem_upgrade.sh
@@ -27,6 +27,7 @@ set -o pipefail
 # Import shared functions
 # shellcheck disable=SC1091
 source "$(dirname "${0}")/functions.sh"
+# shellcheck disable=SC1091
 source "$(dirname "${0}")/upgrade_postgres.sh"
 
 ### Constants
@@ -47,6 +48,7 @@ si_config_repo="edge-manageability-framework"
 apps_ns=onprem
 argo_cd_ns=argocd
 gitea_ns=gitea
+# shellcheck disable=SC2034
 root_app=root-app
 
 # Variables that depend on the above and might require updating later, are placed in here
@@ -163,6 +165,7 @@ check_orch_install() {
             exit 1
         fi
 
+        # shellcheck disable=SC2034
         installed_ver=$(dpkg-query -W -f='${Version}' "$package_name")
         incoming_ver="${package#*:}"
         incoming_ver="${incoming_ver#v}"
@@ -381,6 +384,7 @@ EOF
 ##### UPGRADE SCRIPT START #####
 ################################
 
+# shellcheck disable=SC2034
 while getopts 'v:hbol' flag; do
     case "${flag}" in
     h) HELP='true' ;;
@@ -399,13 +403,13 @@ fi
 
 # Check if postgres is running and if it is safe to backup
 check_postgres
-if [[ $? -ne 0 ]]; then
+if ! check_postgres; then
     echo "PostgreSQL is not running or backup file already exists. Exiting..."
     exit 1
 fi
 
 # Perform postgreSQL backup
-kubectl get secret -n $postgres_namespace postgresql -o yaml > postgres_secret.yaml
+kubectl get secret -n orch-database postgresql -o yaml > postgres_secret.yaml
 
 # delete gitea secrets before backup
 cleanup_gitea_secrets
@@ -679,7 +683,7 @@ set +e
 while true; do
     echo "Checking PostgreSQL pod status..."
     podname=$(kubectl get pods -n orch-database -l app.kubernetes.io/name=postgresql -o jsonpath='{.items[0].metadata.name}')
-    pod_status=$(kubectl get pods -n orch-database $podname -o jsonpath='{.status.phase}')
+    pod_status=$(kubectl get pods -n orch-database "$podname" -o jsonpath='{.status.phase}')
     if [[ "$pod_status" == "Running" ]]; then
         echo "PostgreSQL pod is Running."
         sleep 30
