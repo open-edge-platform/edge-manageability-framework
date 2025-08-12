b838b0716d43036ffaea448d28d304e781aef05d
Andrei Palade <andrei.palade@intel.com>
2025-08-12 13:37:28 +0100

    onprem disable open port (#672)

--- on-prem-installers/rke2/rke2installerlocal.sh
+++ on-prem-installers/rke2/rke2installerlocal.sh
@@ -115,6 +115,7 @@ disable:
   - rke2-ingress-nginx
   - rke2-snapshot-controller
   - rke2-snapshot-validation-webhook
+disable-cloud-controller: true  
 kubelet-arg:
   - "max-pods=200"
 etcd-arg:
