14def26a156c2183ca739a12efac56f30c7e1d77
Goyal, Pankaj <pankaj.goyal@intel.com>
2025-09-14 12:45:54 +0530

    Static user 'vault-user' use for KMS

--- pod-configs/module/kms/main.tf
+++ pod-configs/module/kms/main.tf
@@ -3,13 +3,23 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Set up IAM user for Vault to access KMS
-resource "aws_iam_user" "vault" {
-  name = "vault-${var.cluster_name}"
-  permissions_boundary = var.permissions_boundary != "" ? var.permissions_boundary : null
+#resource "aws_iam_user" "vault" {
+#  name = "vault-${var.cluster_name}"
+#  permissions_boundary = var.permissions_boundary != "" ? var.permissions_boundary : null
+#}
+
+#resource "aws_iam_access_key" "vault" {
+#  user = aws_iam_user.vault.name
+#}
+
+# Reference existing statically created IAM user
+data "aws_iam_user" "vault" {
+  user_name = "vault-user"
 }
 
+# Create new access key for the existing user
 resource "aws_iam_access_key" "vault" {
-  user = aws_iam_user.vault.name
+  user = data.aws_iam_user.vault.user_name
 }
 
 # Set up KMS key with alias
@@ -21,37 +31,59 @@ resource "aws_kms_key" "vault" {
 resource "aws_kms_alias" "vault" {
   name          = "alias/vault-kms-unseal-${var.cluster_name}"
   target_key_id = aws_kms_key.vault.key_id
+
+  depends_on = [aws_kms_key.vault]
 }
 
+#resource "aws_kms_key_policy" "vault" {
+#  key_id = aws_kms_key.vault.id
+#  policy = jsonencode({
+#    Id = "vault"
+#    Statement = [
+#      {
+#          "Sid": "Enable IAM User Permissions",
+#          "Effect": "Allow",
+#          "Principal": {
+#              "AWS": "arn:aws:iam::${var.aws_account_number}:root"
+#          },
+#          "Action": "kms:*",
+#          "Resource": "*"
+#      },
+#      {
+#        "Sid": "Allow use of the key"
+#        "Action": [
+#          "kms:Encrypt",
+#          "kms:Decrypt",
+#          "kms:ReEncrypt*",
+#          "kms:GenerateDataKey*",
+#          "kms:DescribeKey"
+#        ]
+#        "Effect": "Allow"
+#        "Principal": {
+#          "AWS": "arn:aws:iam::${var.aws_account_number}:user/${aws_iam_user.vault.name}"
+#        }
+#        "Resource": "*"
+#      },
+#    ]
+#    Version = "2012-10-17"
+#  })
+#}
+
+# KMS key policy - only root permissions, no user-specific policies
 resource "aws_kms_key_policy" "vault" {
   key_id = aws_kms_key.vault.id
   policy = jsonencode({
     Id = "vault"
     Statement = [
       {
-          "Sid": "Enable IAM User Permissions",
-          "Effect": "Allow",
-          "Principal": {
-              "AWS": "arn:aws:iam::${var.aws_account_number}:root"
-          },
-          "Action": "kms:*",
-          "Resource": "*"
-      },
-      {
-        "Sid": "Allow use of the key"
-        "Action": [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey*",
-          "kms:DescribeKey"
-        ]
-        "Effect": "Allow"
+        "Sid": "Enable IAM User Permissions",
+        "Effect": "Allow",
         "Principal": {
-          "AWS": "arn:aws:iam::${var.aws_account_number}:user/${aws_iam_user.vault.name}"
-        }
+          "AWS": "arn:aws:iam::${var.aws_account_number}:root"
+        },
+        "Action": "kms:*",
         "Resource": "*"
-      },
+      }
     ]
     Version = "2012-10-17"
   })
