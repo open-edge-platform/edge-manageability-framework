036af1099138a3b972abfa1552e3b81ed2880703
Tan, Jian Chern <jian.chern.tan@intel.com>
2025-11-06 15:46:06 +0800

    testing with smtp.intel.com

--- e2e-tests/installer/auto_install.py
+++ e2e-tests/installer/auto_install.py
@@ -566,8 +566,10 @@ class AutoInstall:
         time.sleep(120)
         # in provision config editor
 
-        self.installer_session.sendline('\x1bG$osmtp_url="10.138.167.51"\x1b')
-        self.installer_session.sendline('\x1bG$osmtp_port=587\x1b')
+        self.installer_session.sendline('\x1bG$osmtp_url="smtp.intel.com"\x1b')
+        self.installer_session.sendline('\x1bG$osmtp_port=25\x1b')
+        # self.installer_session.sendline('\x1bG$osmtp_url="10.138.167.51"\x1b')
+        # self.installer_session.sendline('\x1bG$osmtp_port=587\x1b')
         self.installer_session.sendline('\x1bG$osmtp_from="kevin test <kevin@test.com>"\x1b')
 
         self.installer_session.sendline(":wq")
