e2c091d7e77f8ea6f8522ab5f331d809aec83b03
OLoughlin, John <john.oloughlin@intel.com>
2025-12-10 07:18:38 -0800

    remove vol pvc when deleteing cluster
    Signed-off-by: OLoughlin, John <john.oloughlin@intel.com>

--- pod-configs/utils/provision.sh
+++ pod-configs/utils/provision.sh
@@ -1157,7 +1157,26 @@ EOF
         connect_cluster
         kubectl --kubeconfig "${KUBECONFIG}" patch storageclass gp2 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
     fi
-
+    if action == "destroy" ; then
+      # Check if variable is not empty
+      if [[ -n "$ENV_NAME" ]]; then
+        volumes=$(aws ec2 describe-volumes \
+          --region "$AWS_REGION" \
+          --filters "Name=tag:kubernetes.io/cluster/${ENV_NAME},Values=owned" \
+          --query 'Volumes[*].VolumeId' \
+          --output text)
+        echo $volumes
+        if [ -n "$volumes" ]; then
+          echo "Found orphaned volumes: $volumes"
+          for vol in $volumes; do
+              echo "Deleting volume: $vol"
+              aws ec2 delete-volume --region "$AWS_REGION" --volume-id "$vol"
+          done
+        else
+          echo "No orphaned volumes found"
+        fi
+      fi
+    fi
     rm -rf $dir
 }
 
