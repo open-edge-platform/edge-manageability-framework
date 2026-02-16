dcb0d5755d39cbb103617cf0618418ca0abd7801
Joanna Kossakowska <joanna.kossakowska@intel.com>
2026-02-16 05:19:44 -0800

    Change var name to EIM_NOOBB_PROFILE

--- installer/generate_cluster_yaml.sh
+++ installer/generate_cluster_yaml.sh
@@ -68,7 +68,7 @@ export EMAIL_PROFILE='- orch-configs/profiles/alerting-emails.yaml'
 export ARTIFACT_RS_PROFILE='- orch-configs/profiles/artifact-rs-production-noauth.yaml'
 export OSRM_MANUAL_PROFILE='- orch-configs/profiles/enable-osrm-manual-mode.yaml'
 export RESOURCE_DEFAULT_PROFILE='- orch-configs/profiles/resource-default.yaml'
-export EIM_SCENARIO_PROFILE='#- orch-configs/profiles/eim-scenario-noobb.yaml'
+export EIM_NOOBB_PROFILE='#- orch-configs/profiles/eim-noobb.yaml'
 
 if [[ "${ORCH_INSTALLER_PROFILE:-}" == "onprem-vpro" || "${ORCH_INSTALLER_PROFILE:-}" == "aws-vpro" ]]; then
   export DISABLE_AO_PROFILE=true
@@ -214,11 +214,11 @@ if [ "$DEPLOY_TYPE" = "onprem" ]; then
     if [ "${DISABLE_O11Y_PROFILE:-false}" = "true" ]; then
         export O11Y_ENABLE_PROFILE="#- orch-configs/profiles/enable-o11y.yaml"
         export O11Y_PROFILE="#- orch-configs/profiles/o11y-onprem.yaml"
-        export EIM_SCENARIO_PROFILE="- orch-configs/profiles/eim-scenario-noobb.yaml"
+        export EIM_NOOBB_PROFILE="- orch-configs/profiles/eim-noobb.yaml"
     else
         export O11Y_ENABLE_PROFILE="- orch-configs/profiles/enable-o11y.yaml"
         export O11Y_PROFILE="- orch-configs/profiles/o11y-onprem.yaml"
-        export EIM_SCENARIO_PROFILE="#- orch-configs/profiles/eim-scenario-noobb.yaml"
+        export EIM_NOOBB_PROFILE="#- orch-configs/profiles/eim-noobb.yaml"
     fi
 
     if [ "${CLUSTER_SCALE_PROFILE}" = "1ken" ]; then
@@ -270,11 +270,11 @@ elif [ "$DEPLOY_TYPE" = "aws" ]; then
     if [ "${DISABLE_O11Y_PROFILE:-false}" = "true" ]; then
         export O11Y_ENABLE_PROFILE="#- orch-configs/profiles/enable-o11y.yaml"
         export O11Y_PROFILE="#- orch-configs/profiles/o11y-release.yaml"
-        export EIM_SCENARIO_PROFILE="- orch-configs/profiles/eim-scenario-noobb.yaml"
+        export EIM_NOOBB_PROFILE="- orch-configs/profiles/eim-noobb.yaml"
     else
         export O11Y_ENABLE_PROFILE="- orch-configs/profiles/enable-o11y.yaml"
         export O11Y_PROFILE="- orch-configs/profiles/o11y-release.yaml"
-        export EIM_SCENARIO_PROFILE="#- orch-configs/profiles/eim-scenario-noobb.yaml"
+        export EIM_NOOBB_PROFILE="#- orch-configs/profiles/eim-noobb.yaml"
         if [[ "$CLUSTER_SCALE_PROFILE" =~ ^(500en|1ken|10ken)$ ]]; then
             export O11Y_PROFILE="- orch-configs/profiles/o11y-release-large.yaml"
         fi
