6f77268d2ca9f8e037e71fffb5eb7027ad388203
PalashGoelIntel <palash.goel@intel.com>
2026-02-23 16:27:00 +0530

    Update onprem_orch_install.sh

--- on-prem-installers/onprem/onprem_orch_install.sh
+++ on-prem-installers/onprem/onprem_orch_install.sh
@@ -215,7 +215,7 @@ reset_runtime_variables() {
   
   # Unset variables in current shell
   unset SRE_TLS_ENABLED SRE_DEST_CA_CERT SMTP_SKIP_VERIFY
-  unset DISABLE_CO_PROFILE DISABLE_AO_PROFILE DISABLE_O11Y_PROFILE
+  unset DISABLE_CO_PROFILE DISABLE_AO_PROFILE DISABLE_O11Y_PROFILE SINGLE_TENANCY_PROFILE
   
   echo "Runtime variables cleaned successfully."
 }
