4c23837e5d5ba3dc294ba178049d1f66dede70aa
Baker, Scott <scott.baker@intel.com>
2026-02-24 15:31:10 -0800

    propagate value into gitea tf

--- pod-configs/module/gitea/variable.tf
+++ pod-configs/module/gitea/variable.tf
@@ -27,6 +27,12 @@ variable "gitea_fqdn" {
   description = "The domain of the Gitea service"
 }
 
+# If install_from_local_gitea is true, argo will install from the local gitea
+variable "install_from_local_gitea" {
+  type    = bool
+  default = true
+}
+
 # Uncomment following variables when switching to RDS
 # variable "gitea_database_endpoint" {
 #   description = "The endpoint of the Gitea database"
