36a76e4563ab41c1ab0e68cb5c43e525a0011f4b
Baker, Scott <scott.baker@intel.com>
2026-02-26 16:36:08 -0800

    add variables for DEPLOY_REPO_URL and DEPLOY_REPO_BRANCH

--- installer/configure-cluster.sh
+++ installer/configure-cluster.sh
@@ -88,6 +88,8 @@ export NGINX_TG_ARN
 export ARGOCD_TG_ARN
 export S3_PREFIX
 export INSTALL_FROM_LOCAL_GITEA
+export DEPLOY_REPO_URL
+export DEPLOY_REPO_BRANCH
 
 # For ORCH upgrade, check the existing deployment 'root-app'
 
