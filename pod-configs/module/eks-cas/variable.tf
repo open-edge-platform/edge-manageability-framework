a98997f36e965ac38db278305fc59cfe83131b1c
hemasenthil13 <hemapriya.senthilkumar@intel.com>
2026-02-28 13:34:32 +0530

    feat: Update variable.tf
    docs: done

--- pod-configs/module/eks-cas/variable.tf
+++ pod-configs/module/eks-cas/variable.tf
@@ -7,6 +7,11 @@ variable "cas_namespace" {
   default     = "kube-system"
 }
 
+variable cas_version {
+  type = string
+  default = "9.46.6"
+}
+
 variable "cas_service_account" {
   description = "The Kubernetes service account name of cluster autoscaler"
   default     = "cluster-autoscaler"
@@ -18,4 +23,19 @@ variable "cas_controller_arn" {
 
 variable "aws_region" {
   type = string
+}
+
+variable "CAS_HP" {
+  type    = string
+  default = ""
+}
+
+variable "CAS_HPS" {
+  type    = string
+  default = ""
+}
+
+variable "CAS_NP" {
+  type    = string
+  default = ""
 }
