2f4ec0db8f94f0ca45b07afcb7cb0603098f6834
Hardik Windlass <108932456+hwindlas@users.noreply.github.com>
2026-03-20 07:47:51 +0530

    Update Version match regex in component_status_test

--- e2e-tests/orchestrator/component_status_test.go
+++ e2e-tests/orchestrator/component_status_test.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -693,7 +692,7 @@ var _ = Describe("Component Status Service", Label(componentStatusLabel, orchest
 			version := status.Orchestrator.Version
 			Expect(version).ToNot(BeEmpty(), "version should not be empty")
 			// Version can be: 2026.0, 2026.0-dev, 2026.0.0, 2026.0.0-dev-hash, v2026.0.0
-			Expect(strings.TrimSpace(version)).To(MatchRegexp(`^v?\d{4}\.\d+(\.\d+)?(-[a-z]+(-[a-f0-9]+)?)?$`),
+			Expect(version).To(MatchRegexp(`^v?\d{4}\.\d+(\.\d+)?(-([a-z]+(-[a-f0-9]+)?|[a-f0-9]+))?$`)),
 				fmt.Sprintf("version format should be valid: %s", version))
 		})
 
