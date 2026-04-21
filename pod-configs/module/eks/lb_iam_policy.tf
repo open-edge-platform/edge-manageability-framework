fb13b076ad79bc6804f16c2c185c2abb42db6300
Sunil Parida <sunil.kumar.parida@intel.com>
2026-04-21 10:56:27 +0530

    cleanup repo

--- pod-configs/module/eks/lb_iam_policy.tf
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Intel Corporation
-#
-# SPDX-License-Identifier: Apache-2.0
-
-resource "aws_iam_policy" "aws_load_balancer" {
-  name   = "${var.cluster_name}-aws-load-balancer"
-  policy = file("${path.module}/lb_policy.json")
-}
